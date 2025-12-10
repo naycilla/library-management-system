42ea0bd0e9e8392d3e0b13dece530f97bde91da4
naycilla <shafa.naycilla@binus.ac.id>
2025-12-10 11:43:23 +0700

    docker error fixing

--- db/init.sql
+++ db/init.sql
@@ -1,32 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Waktu pembuatan: 10 Des 2025 pada 05.09
--- Versi server: 10.4.32-MariaDB
--- Versi PHP: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `perpus`
---
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `anggota`
---
-
 CREATE TABLE `anggota` (
   `id_anggota` int(11) NOT NULL,
   `nama` varchar(50) NOT NULL,
--- src/config/db.php
+++ src/config/db.php
@@ -1,7 +1,7 @@
 <?php
-    $host = "localhost";
+    $host = "db";
     $user = "root";
-    $pass = "";
+    $pass = "root";
     $db   = "perpus";
 
     $koneksi = mysqli_connect($host, $user, $pass, $db);
